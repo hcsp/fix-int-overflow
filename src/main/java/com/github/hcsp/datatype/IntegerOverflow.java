1db5c0a1980f464476b5f1e80ad8b12c1313c84d
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
+++ src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
@@ -1,8 +1,8 @@
 package com.github.hcsp.datatype;
 
-// 修复这个方法里的问题，使得它正确输出 "i=3000000000"
-// Fix this method to make it output "i=3000000000"
 public class IntegerOverflow {
+    // 修复这个方法里的问题，使得它正确输出 "i=3000000000"
+    // Fix this method to make it output "i=3000000000"
     public static void main(String[] args) {
         int 十亿 = 10_0000_0000;
         int i = 0;
--- src/test/java/com/github/hcsp/datatype/IntegerOverflowTest.java
+++ src/test/java/com/github/hcsp/datatype/IntegerOverflowTest.java
@@ -1,17 +1,16 @@
 package com.github.hcsp.datatype;
 
-import static org.hamcrest.Matchers.containsString;
-
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class IntegerOverflowTest {
     @Test
     @CaptureSystemOutput
     public void printNumbersCorrectly(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(containsString("i=1000000000"));
-        capture.expect(containsString("i=2000000000"));
-        capture.expect(containsString("i=3000000000"));
+        capture.expect(Matchers.containsString("i=1000000000"));
+        capture.expect(Matchers.containsString("i=2000000000"));
+        capture.expect(Mathcers.containsString("i=3000000000"));
         IntegerOverflow.main(null);
     }
 }

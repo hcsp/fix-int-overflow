721fc4ecaaa923811a76a2ec50b3bd41f4d3e0cc
gaozhidong <1011284295@qq.com>
2019-07-20 15:31:02 +0800

    合适的类型对应合适的数字

--- src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
+++ src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
@@ -5,8 +5,10 @@ public class IntegerOverflow {
     // Fix this method to make it output "i=3000000000"
     public static void main(String[] args) {
         int 十亿 = 10_0000_0000;
-        int i = 0;
-
+        System.out.println( Long.MAX_VALUE);
+        System.out.println( Integer.MAX_VALUE);
+//        int i = 0;
+        long i = 0;
         i = i + 十亿;
         System.out.println("i=" + i);
         i = i + 十亿;

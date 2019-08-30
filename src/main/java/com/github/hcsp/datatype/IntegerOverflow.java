c4f367cbfe86814b87d0855cb9a61d9546f93f7f
Mcavoy GENG <gigi834921748@qq.com>
2019-08-30 21:06:28 +0800

    把int类型换成long类型 (#62)
    把int类型换成long类型 (#62)

--- src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
+++ src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
@@ -4,8 +4,8 @@ public class IntegerOverflow {
     // 修复这个方法里的问题，使得它正确输出 "i=3000000000"
     // Fix this method to make it output "i=3000000000"
     public static void main(String[] args) {
-        int 十亿 = 10_0000_0000;
-        int i = 0;
+        long 十亿 = 10_0000_0000L;
+        long i = 0;
 
         i = i + 十亿;
         System.out.println("i=" + i);

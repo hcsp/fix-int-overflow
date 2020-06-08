5985c22151c281ff4d20580d6f9fa0b47a9a1bca
Leo <790868516@qq.com>
2020-06-08 20:23:24 +0800

    修复整数的溢出问题

--- src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
+++ src/main/java/com/github/hcsp/datatype/IntegerOverflow.java
@@ -4,13 +4,13 @@ public class IntegerOverflow {
     // 修复这个方法里的问题，使得它正确输出 "i=3000000000"
     // Fix this method to make it output "i=3000000000"
     public static void main(String[] args) {
-        int 十亿 = 10_0000_0000;
+        int billion = 10_0000_0000;
         long i = 0;
-        i = i + 十亿;
+        i = i + billion;
         System.out.println("i=" + i);
-        i = i + 十亿;
+        i = i + billion;
         System.out.println("i=" + i);
-        i = i + 十亿;
+        i = i + billion;
         System.out.println("i=" + i);
     }
 }
